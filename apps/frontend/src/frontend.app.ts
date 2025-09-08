4d443d753633b4d59957bb87128561f129ae6e87
Jacob Hands <jacob@jacobhands.com>
2025-09-08 07:28:12 -0500

    fix: hardcode public api url to fetch

--- apps/frontend/src/frontend.app.ts
+++ apps/frontend/src/frontend.app.ts
@@ -24,7 +24,7 @@ const app = new Hono<App>()
 	})
 
 	.get('/public-api', async (c) => {
-		const res = await c.env.PUBLIC_API.fetch(c.req.raw)
+		const res = await c.env.PUBLIC_API.fetch('http://public-api/')
 		return c.newResponse(res.body, res)
 	})
 
