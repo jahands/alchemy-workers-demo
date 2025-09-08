f0253b554dc514d782daf3c4e2f0defa7361a5b2
Jacob Hands <jacob@jacobhands.com>
2025-09-08 06:54:20 -0500

    chore: change import from /infra to /resources

--- apps/infra/alchemy.run.ts
+++ apps/infra/alchemy.run.ts
@@ -1,7 +1,7 @@
 import alchemy from 'alchemy'
 import { CloudflareStateStore } from 'alchemy/state'
 
-import { PublicApi } from '@repo/public-api/infra'
+import { PublicApi } from '@repo/public-api/resources'
 
 const app = await alchemy('acme', {
 	stateStore: (scope) => new CloudflareStateStore(scope),
