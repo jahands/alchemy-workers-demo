1bacf5b0e908ed20512c42b3b7b717e878b5f1e3
Jacob Hands <jacob@jacobhands.com>
2025-09-09 04:57:05 -0500

    fix: allow alchemy to use username

--- packages/acme-common/src/acme-common.ts
+++ packages/acme-common/src/acme-common.ts
@@ -1,4 +1,5 @@
+import os from 'node:os'
 import { z } from 'zod'
 
 export type AcmeStage = z.infer<typeof AcmeStage>
-export const AcmeStage = z.enum(['prod', 'dev'])
+export const AcmeStage = z.enum(['prod', 'dev', os.userInfo().username])
