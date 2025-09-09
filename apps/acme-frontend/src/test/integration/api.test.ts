665fe67644c2435f5670c7ccdc2f5ca737c145e3
Jacob Hands <jacob@jacobhands.com>
2025-09-09 05:48:08 -0500

    feat: add tests

--- apps/acme-frontend/src/test/integration/api.test.ts
+++ apps/acme-frontend/src/test/integration/api.test.ts
@@ -1,8 +1,20 @@
 import { SELF } from 'cloudflare:test'
-import { expect, it } from 'vitest'
+import { expect, test } from 'vitest'
 
-it('response with hello world', async () => {
-	const res = await SELF.fetch('https://example.com')
+test('GET /', async () => {
+	const res = await SELF.fetch('https://acme-frontend')
 	expect(res.status).toBe(200)
 	expect(await res.text()).toMatchInlineSnapshot(`"hello from acme-frontend!"`)
+})
+
+test('GET /acme-api', async () => {
+	const res = await SELF.fetch('https://acme-frontend/acme-api')
+	expect(res.status).toBe(200)
+	expect(await res.text()).toMatchInlineSnapshot(`"hello from acme-api!"`)
+})
+
+test('GET /acme-api/add', async () => {
+	const res = await SELF.fetch('https://acme-frontend/acme-api/add')
+	expect(res.status).toBe(200)
+	expect(await res.text()).toMatchInlineSnapshot(`"3"`)
 })
