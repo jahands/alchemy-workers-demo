a3b6a71be318435e1ab2d70aae1d8e8da1f166e3
Jacob Hands <jacob@jacobhands.com>
2025-09-09 05:46:35 -0500

    fix: add dependent Workers

--- apps/acme-frontend/src/test/integration/api.test.ts
+++ apps/acme-frontend/src/test/integration/api.test.ts
@@ -4,5 +4,5 @@ import { expect, it } from 'vitest'
 it('response with hello world', async () => {
 	const res = await SELF.fetch('https://example.com')
 	expect(res.status).toBe(200)
-	expect(await res.text()).toMatchInlineSnapshot(`"hello, world!"`)
+	expect(await res.text()).toMatchInlineSnapshot(`"hello from acme-frontend!"`)
 })
--- apps/acme-frontend/vitest.config.ts
+++ apps/acme-frontend/vitest.config.ts
@@ -1,4 +1,45 @@
+import { execSync } from 'node:child_process'
+import fs from 'node:fs'
 import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
+import z from 'zod'
+
+const WranglerJson = z.object({
+	name: z.string(),
+	main: z.string(),
+	compatibility_date: z.string(),
+	compatibility_flags: z.array(z.string()),
+	r2_buckets: z
+		.array(
+			z.object({
+				binding: z.string(),
+				bucket_name: z.string(),
+			})
+		)
+		.optional(),
+})
+
+async function getWorker(pkgName: string) {
+	const pkgDir = `${__dirname}/node_modules/${pkgName}`
+	const wranglerJsonRaw = fs.readFileSync(`${pkgDir}/wrangler.jsonc`, 'utf8')
+	const wranglerJson = WranglerJson.parse(JSON.parse(wranglerJsonRaw))
+	execSync('bun wrangler build', { cwd: pkgDir })
+	return {
+		name: wranglerJson.name,
+		scriptPath: `${pkgDir}/dist/${wranglerJson.main.replace(/\.ts$/, '.js').replace(/^src\//, '')}`,
+		modules: true,
+		compatibilityDate: wranglerJson.compatibility_date,
+		compatibilityFlags: wranglerJson.compatibility_flags,
+		r2Buckets: wranglerJson.r2_buckets?.reduce(
+			(acc, bucket) => {
+				acc[bucket.binding] = bucket.bucket_name
+				return acc
+			},
+			{} as Record<string, string>
+		),
+	}
+}
+
+await getWorker('@repo/acme-api')
 
 export default defineWorkersProject({
 	test: {
@@ -9,6 +50,10 @@ export default defineWorkersProject({
 					bindings: {
 						ENVIRONMENT: 'VITEST',
 					},
+					serviceBindings: {
+						ACME_API: 'acme-api-worker-dev',
+					},
+					workers: [await getWorker('@repo/acme-api')],
 				},
 			},
 		},
